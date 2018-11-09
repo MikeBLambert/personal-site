109cfd8c97835aea05487429754137efb2c56cb8
MikeBLambert <mlamb128@gmail.com>
2018-11-09 09:47:00 -0800

    fadeIn function working for portfolio projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
-window.onscroll = function() {makeFixed(), fadeIn()};
 
-let header = document.getElementById('myHeader');
-let nav = document.getElementById('desktop-nav');
-let portfolio = document.getElementById('portfolio');
+const header = document.getElementById('myHeader');
+const nav = document.getElementById('desktop-nav');
 
-let project = document.getElementsByClassName('project')
+const portfolio = document.getElementById('portfolio');
+const projects = document.getElementsByClassName('project');
+
 
 function makeFixed() {
     if(window.pageYOffset >= header.offsetHeight) {
         nav.classList.add('fix');
         portfolio.classList.add('portfolioOffset');
-
+        
     } else if (header.offsetHeight > window.pageYOffset) {
         nav.classList.remove('fix');
         portfolio.classList.remove('portfolioOffset')
     }
 }
 
-function fadeIn() {
-    if(window.pageYOffset >= nav.offsetHeight) {
-        project[0].style.animationName = 'fade-in';
-        project[0].style.animationDuration = '5s';
-    } else {
-        // console.log(project[0].style)
-        project[0].style.display = 'none';
+const fadeIn = function (parent, children) {
+    if(window.pageYOffset + window.innerHeight >= parent.offsetHeight) {
+        Array.from(children).forEach((child, index) => {
+            child.classList.add('fade-in');
+            child.classList.remove('display-none');
+        })
     }
 }
+
+window.onscroll = function() {
+    makeFixed(), 
+    fadeIn(portfolio, projects)
+};
+
 
