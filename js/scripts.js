97b109497da1986048bb826bced68a7f4ec7b818
MikeBLambert <mlamb128@gmail.com>
2018-11-12 06:47:46 -0800

    Modal button with overlay fully working. Still need to work on positioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 
-const header = document.getElementById('myHeader');
 const nav = document.getElementById('desktop-nav');
 
 // const portfolio = document.getElementById('portfolio');
@@ -34,49 +33,56 @@ const nav = document.getElementById('desktop-nav');
 //     fadeIn(portfolio, projects),
 //     fadeIn(contact, contactIcons)
 // };
+const header = document.getElementById('myHeader');
+const about = document.getElementById('about');
+const blog = document.getElementById('blog');
+const contact = document.getElementById('contact');
 
 const modals = document.getElementsByClassName('modal');
 const projects = document.getElementsByClassName('project');
 const portfolio = document.getElementById('portfolio');
-const main = document.getElementById('main');
 const overlay = document.getElementById('overlay');
 
 for(let i=0; i<projects.length; i++) {
+
     const clickedProject = projects[i];
+    const projectDetails = clickedProject.childNodes[3];
 
     clickedProject.onclick = function() {
-        this.classList.toggle('modal-item');
-        this.childNodes[3].classList.toggle('show-content');
-        main.classList.toggle('overlay');
-        portfolio.classList.toggle('modal-view')
+        this.classList.add('modal-item');
+        projectDetails.classList.add('show-content');
+        portfolio.classList.add('modal-view')
         portfolio.style.display = 'flex';
         overlay.style.display = 'block';
+
+        // header.classList.add('hidden');
+        // about.classList.add('hidden');
+        // blog.classList.add('hidden');
+        // contact.classList.add('hidden');
+
         Array.from(portfolio.children).forEach((element) => {
             if(element !== clickedProject) {
-                element.classList.toggle('hidden');
+                element.classList.add('hidden');   
             }
         })
     }
-
 }
-        main.onclick = function() {
-            console.log('hi')
-        }
 
+overlay.onclick = function() {
+    // header.classList.remove('hidden');
+    // about.classList.remove('hidden');
+    // blog.classList.remove('hidden');
+    // contact.classList.remove('hidden');
+     
+    portfolio.classList.remove('modal-view');
+    portfolio.style.display = 'grid';
+    overlay.style.display = 'none';
+    Array.from(portfolio.children).forEach(element => {
+        element.classList.remove('hidden');
+        element.classList.remove('modal-item');
+        element.childNodes[3].classList.remove('show-content');
+    })
+}
 
-// trigger.addEventListener('click', toggleModal);
-// const closeModal = function() {
-//     modal.style.display = 'none';
-// }
-
-// button.addEventListener('click', openModal, false);
-
-
-// window.onclick = function(event) {
-//     console.log(clickable)
-//     if(event.target === button) {
-//         console.log('hi')
-//     }
-// }
 
 
